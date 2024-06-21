b72b7d8fdf22d82617667bd364aadda38112b259
steve <stevechap405@gmail.com>
2024-06-21 16:41:27 +0200

    update

--- src/Components/testSearch/Crypto.tsx
+++ src/Components/testSearch/Crypto.tsx
@@ -1,31 +1,31 @@
-import React from "react";
+// import React from "react";
 
-import { CryptoProps } from "@/Utils/type";
+// import { CryptoProps } from "@/Utils/type";
 
-const Crypto = ({ name, image, value, quantity }: CryptoProps) => {
-  return (
-    <div className="group relative">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-        <img
-          src={image}
-          alt="Front of men&#039;s Basic Tee in black."
-          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-        />
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm text-gray-700">
-            <a href="#">
-              <span aria-hidden="true" className="absolute inset-0"></span>
-              {name}
-            </a>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{value}</p>
-        </div>
-        <p className="text-sm font-medium text-gray-900">${quantity}</p>
-      </div>
-    </div>
-  );
-};
+// const Crypto = ({ name, image, value, quantity }: CryptoProps) => {
+//   return (
+//     <div className="group relative">
+//       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+//         <img
+//           src={image}
+//           alt="Front of men&#039;s Basic Tee in black."
+//           className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+//         />
+//       </div>
+//       <div className="mt-4 flex justify-between">
+//         <div>
+//           <h3 className="text-sm text-gray-700">
+//             <a href="#">
+//               <span aria-hidden="true" className="absolute inset-0"></span>
+//               {name}
+//             </a>
+//           </h3>
+//           <p className="mt-1 text-sm text-gray-500">{value}</p>
+//         </div>
+//         <p className="text-sm font-medium text-gray-900">${quantity}</p>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Crypto;
+// export default Crypto;
--- src/Components/testSearch/warpper.tsx
+++ src/Components/testSearch/warpper.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+// import React from "react";
 
-const Wrapper = ({ children }: any) => {
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        {children}
-      </div>
-    </div>
-  );
-};
+// const Wrapper = ({ children }: any) => {
+//   return (
+//     <div className="bg-white">
+//       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Wrapper;
+// export default Wrapper;
